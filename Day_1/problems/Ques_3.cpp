217cd168e72d4af9bbe4ada3447b08c6b54b81cf
Armaan singh bhau <armaansinghbhau8@gmail.com>
2025-03-09 12:33:48 +0530

    Perform calulcations on two different type of datatypes and resulted as one of them

---
+++ Day_1/problems/Ques_3.cpp
@@ -0,0 +1,16 @@
+// Write a C++ program that performs arithmetic operations (addition, subtraction, 
+//multiplication, division) on two numbers: one int and one float. Print the result as a float
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cin >> a;
+
+    float b;
+    cin >> b;
+
+    float c = a + b;
+    cout << c << endl;
+}
